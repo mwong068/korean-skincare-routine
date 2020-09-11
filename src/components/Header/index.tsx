21b3617f6484643c9255c785a9d1d19085f6a657
mwong068 <mwong068@gmail.com>
2020-09-11 16:12:09 -0700

    added header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,64 @@
 import React from 'react';
+import styled from 'styled-components';
 import Nav from '../Nav';
+import img from '../../assets/bg-hero-desktop.svg';
+
+const Background = styled.div`
+    background-image: url(${img});
+    background-color: hsl(193, 100%, 96%);
+`;
+
+const HeaderText = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    padding: 10px 0 30px 0;
+`;
+
+const LeftText = styled.div`
+    display: grid;
+    text-align: left;
+    width: 300px;
+    padding: 20px;
+
+    p {
+        font-size: 12px;
+    }
+`;
+
+const RightImages = styled.div`
+    width: 450px;
+    img {
+        width: 100%;
+    }
+`;
+
+const Button = styled.button`
+    height: 40px;
+    width: 205px;
+    border-radius: 50px;
+    background-color: hsl(322, 100%, 66%);
+    color: white;
+    border: 1px solid #f2f2f2;
+    box-shadow: 1px 1px 1px 1px #f2f2f2;
+`;
 
 class Header extends React.Component {
     render() {
         return (
-        <div className="header">
+        <Background>
             <Nav />
-            Header
-        </div>
+            <HeaderText>
+                <LeftText>
+                    <h2>Build The Community<br></br>Your Fans Will Love</h2>
+                    <p>Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussions.</p>
+                    <Button>Get Started For Free</Button>
+                </LeftText>
+                <RightImages>
+                    <img src={require('../../assets/illustration-mockups.svg')} />
+                </RightImages>
+            </HeaderText>
+        </Background>
         )
     }
 }
--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,14 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Navigation = styled.div`
+    padding: 20px;
+
+    ul {
+        display: flex;
+        justify-content: space-between;
+        list-style-type: none;
+        padding: 15px 20px;
+    }
+`;
+
+const Logo = styled.div`
+    img {
+        width: 150px;
+    }
+`;
+
+const Button = styled.button`
+    height: 30px;
+    width: 115px;
+    border-radius: 50px;
+    background-color: white;
+    border: 1px solid hsl(208, 11%, 55%);
+    box-shadow: 1px 1px 1px 1px hsl(208, 11%, 55%);
+`;
 
 class Nav extends React.Component {
     render() {
         return (
-        <div className="nav">
+        <Navigation>
             <ul>
-                <li>Huddle</li>
-                <li><button>Try It Free</button></li>
+                <li><Logo><img src={require('../../assets/logo.svg')} /></Logo></li>
+                <li><Button>Try It Free</Button></li>
             </ul>
-        </div>
+        </Navigation>
         )
     }
 }
